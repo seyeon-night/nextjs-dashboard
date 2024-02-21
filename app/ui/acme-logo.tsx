6f77565b7156d50dddec751c284b0882968f4202
seyeon-night <seyeon-night@gmail.com>
2024-02-21 11:49:40 +0900

    init

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,11 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+//import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div>
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
+    </div>
+  );
+}
